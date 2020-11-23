a2d739c29dd4057a0881584ffa534d01bf8ab85f
Nicholas Leonard <nicholas.leonard@coker.edu>
2020-11-23 02:58:31 +0000

    add flash error message

--- utils/isLoggedIn.js
+++ utils/isLoggedIn.js
@@ -3,6 +3,7 @@ const isLoggedIn=(req,res,next)=> {
 	if(req.isAuthenticated()){
 		return next();
 	}else{
+		req.flash("error", "Hey you must be loggin in to that G");
 		res.redirect("/login");
 	};
 };
